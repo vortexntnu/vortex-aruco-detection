471fcbc02c5fcc5a29bf784df50cf3ebfca5afda
Andreas Kluge Svendsrud <kluge7@users.noreply.github.com>
2025-03-23 17:02:33 +0100

    refactor(pre-commit): apply formatting changes

--- aruco-detector/include/aruco_detector/aruco_file_logger.hpp
+++ aruco-detector/include/aruco_detector/aruco_file_logger.hpp
@@ -10,13 +10,13 @@
 /**
  * @brief Writes a vector of integers to a file.
  *
- * This function takes a filename and a vector of integers as input and writes the integers to the specified file.
+ * This function takes a filename and a vector of integers as input and writes
+ * the integers to the specified file.
  *
  * @param filename The name of the file to write the integers to.
  * @param ids The vector of integers to be written to the file.
  */
 void writeIntsToFile(std::string filename, std::vector<int> ids);
-
 
 /**
  * @brief Converts a ROS2 Time object to a string representation.
--- aruco-detector/src/aruco_file_logger.cpp
+++ aruco-detector/src/aruco_file_logger.cpp
@@ -1,24 +1,24 @@
 #include <aruco_detector/aruco_file_logger.hpp>
 
-void writeIntsToFile(std::string filename, std::vector<int> ids)
-{
-	std::ofstream outputFile(filename);
-	if (outputFile.is_open()) {
-		for (int val : ids) {
-			outputFile << val << ", ";
-		}
-	}
-	else
-        RCLCPP_WARN_STREAM(rclcpp::get_logger("aruco_detector"), "Unable to open file " << filename << ", try creating the missing directory");
-	outputFile.close();
-
+void writeIntsToFile(std::string filename, std::vector<int> ids) {
+    std::ofstream outputFile(filename);
+    if (outputFile.is_open()) {
+        for (int val : ids) {
+            outputFile << val << ", ";
+        }
+    } else
+        RCLCPP_WARN_STREAM(rclcpp::get_logger("aruco_detector"),
+                           "Unable to open file "
+                               << filename
+                               << ", try creating the missing directory");
+    outputFile.close();
 }
 
-std::string ros2TimeToString(const rclcpp::Time& time)
-{
-    auto epoch = std::chrono::seconds{0}; // UNIX epoch start
+std::string ros2TimeToString(const rclcpp::Time& time) {
+    auto epoch = std::chrono::seconds{0};  // UNIX epoch start
     auto time_since_epoch = std::chrono::nanoseconds{time.nanoseconds()};
-    auto time_point = std::chrono::time_point<std::chrono::system_clock>{epoch + time_since_epoch};
+    auto time_point = std::chrono::time_point<std::chrono::system_clock>{
+        epoch + time_since_epoch};
 
     // Convert time_point to time_t for compatibility with common time functions
     auto time_t_point = std::chrono::system_clock::to_time_t(time_point);
@@ -30,4 +30,3 @@ std::string ros2TimeToString(const rclcpp::Time& time)
     // Return the formatted string
     return ss.str();
 }
-
